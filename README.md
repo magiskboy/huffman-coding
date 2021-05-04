a30e5ea38beaf51855a701ce8c2cf19a993a0efb
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2021-05-04 09:34:00 +0700

    Update readme

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ Huffman coding implementation
 ```bash
 python example.py
 ```
+Linux
+
+```bash
+$ sudo apt install "graphviz*"
+```
+
+### Demo
+
+![Huffman example](https://firebasestorage.googleapis.com/v0/b/myblog-e552f.appspot.com/o/screenshot%2FScreenshot%20from%202021-05-04%2009-30-50.png?alt=media&token=c267c398-0923-4622-8973-e0597e40a1d9)
+
+Huffman tree
+
+![Huffman tree](https://firebasestorage.googleapis.com/v0/b/myblog-e552f.appspot.com/o/screenshot%2FScreenshot%20from%202021-05-04%2009-30-26.png?alt=media&token=fbb18abc-b3fb-480e-bf53-0191009f4472)
--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 from pprint import pprint
+
 import huffman
 from view import viz_tree
 
-
-data = b'this is an example of a huffman tree'
+data = b"this is an example of a huffman tree"
 
 tree = huffman.build_tree(data)
 map_code = huffman.build_map_code(tree)
@@ -12,17 +12,18 @@ map_code = huffman.build_map_code(tree)
 # encode
 bin_data = huffman.encode(data, map_code)
 
-print('Map code')
-pprint(map_code, width=1)
-print('Encoded data')
-print(bin_data)
+print("Map code")
+for k, v in map_code.items():
+    print("{}: {}".format(chr(k), v.to01()))
+print("Encoded data")
+print(bin_data.to01())
 viz_tree(tree)
 
 # decode
-print('After decode')
+print("After decode")
 print(huffman.decode(bin_data.tobytes(), map_code, bin_data.buffer_info()[3]))
 
 
 # calculate performance
 p = len(bin_data) / (len(data) * 8)
-print(f'Reduce {p * 100}%')
+print(f"Reduce {p * 100}%")
